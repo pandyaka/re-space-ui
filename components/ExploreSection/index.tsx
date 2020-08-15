23ccda319748bb290edde78b740fdc4e472c87ae
Barbariansyah <bariansyahi@gmail.com>
2020-08-16 03:36:44 +0700

    landing props extract

--- components/ExploreSection/index.tsx
+++ components/ExploreSection/index.tsx
@@ -2,14 +2,22 @@ import React, { FunctionComponent } from 'react';
 import { ExploreSectionStyle, SectionCaption, CardGallery } from './style';
 import MallCard from '@components/MallCard';
 import { CenterButton } from '@components/Button';
+import Mall from '@interfaces/mall';
 
-const ExploreSection: FunctionComponent = () => {
+interface MallsMapProps {
+    malls: Mall[];
+}
+
+const ExploreSection: FunctionComponent<MallsMapProps> = (props: MallsMapProps) => {
+    const { malls } = props;
+
     return (
         <ExploreSectionStyle>
             <SectionCaption>Explore Spaces in Tebet</SectionCaption>
             <CardGallery>
-                <MallCard />
-                <MallCard />
+                {malls.map((mall, idx) => (
+                    <MallCard key={idx} id={mall.id} name={mall.name} location={mall.location} />
+                ))}
             </CardGallery>
             <CenterButton ph="see more" href="/malls" w="144px" h="40px" bc="#FF427F" c="white" />
         </ExploreSectionStyle>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,47 @@
 import React, { SFC } from 'react';
+import { GetServerSideProps } from 'next';
+
 import PageLayout from '@components/PageLayout';
 import { LandingLayout } from '@components/Layout';
 import Landing from '@components/Landing';
 import ExploreSection from '@components/ExploreSection';
+import Mall from '@interfaces/mall';
 
-const Index: SFC = () => {
+interface MallsProps {
+    malls: Mall[];
+}
+
+const Index: SFC<MallsProps> = (props: MallsProps) => {
+    const { malls } = props;
     return (
         <PageLayout>
             <LandingLayout>
                 <Landing />
-                <ExploreSection />
+                <ExploreSection malls={malls} />
             </LandingLayout>
         </PageLayout>
     );
 };
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const { res, params } = ctx;
+    const url = `${process.env.API_URL}/malls`;
+
+    try {
+        const res = await fetch(url);
+        const data = (await res.json()) as Mall[];
+
+        return {
+            props: {
+                malls: data
+            }
+        };
+    } catch (e) {
+        res.setHeader('location', '/');
+        res.statusCode = 302;
+        res.end();
+        return { props: {} };
+    }
+};
+
 export default Index;
