51a67e142a2da69984b04dab5f61aab94c8d9730
Barbariansyah <bariansyahi@gmail.com>
2020-08-16 03:46:15 +0700

    extract search props

--- components/SearchResult/index.tsx
+++ components/SearchResult/index.tsx
@@ -3,21 +3,52 @@ import { SectionCaption, CardGallery } from '@components/ExploreSection/style';
 import { SearchSectionStyle } from './style';
 import MallCard from '@components/MallCard';
 import { useRouter } from 'next/router';
+import { GetServerSideProps } from 'next';
+import Mall from '@interfaces/mall';
 
-const SearchResult: FunctionComponent = () => {
+interface MallsProps {
+    malls: Mall[];
+}
+
+const SearchResult: FunctionComponent<MallsProps> = (props: MallsProps) => {
     const router = useRouter();
-    const { location, contractType, spaceType } = router.query;
+    const { location } = router.query;
+    const { malls } = props;
     return (
         <SearchSectionStyle>
             <SectionCaption>Search results for {`"${location}"`}</SectionCaption>
             <CardGallery>
-                <MallCard />
-                <MallCard />
-                <MallCard />
-                <MallCard />
+                {malls.map((mall, idx) => (
+                    <MallCard key={idx} id={mall.id} name={mall.name} location={mall.location} />
+                ))}
             </CardGallery>
         </SearchSectionStyle>
     );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const { res } = ctx;
+    const { location } = ctx.query;
+    const url = `${process.env.API_URL}/malls`;
+
+    try {
+        const res = await fetch(url, {
+            method: 'GET',
+            body: JSON.stringify({ location: location })
+        });
+        const data = (await res.json()) as Mall[];
+
+        return {
+            props: {
+                malls: data
+            }
+        };
+    } catch (e) {
+        res.setHeader('location', '/');
+        res.statusCode = 302;
+        res.end();
+        return { props: {} };
+    }
 };
 
 export default SearchResult;
