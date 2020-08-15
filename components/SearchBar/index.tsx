58531f0682c98224b90c79885c4d102a0f0a90d5
Barbariansyah <bariansyahi@gmail.com>
2020-08-15 20:52:27 +0700

    top search logic

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,22 +1,31 @@
 import React, { FunctionComponent } from 'react';
 import { SearchBarWrapper, SearchField, SearchIcon } from './style';
+import LinkWrapper from '@components/LinkWrapper';
 import { Image } from '@components/Image';
-
-export const SearchBarWithIcon: FunctionComponent = () => {
-    return (
-        <SearchBarWrapper>
-            <SearchIcon>
-                <Image style={{ color: 'red' }} img="/icons/search.svg" w="24px" h="24px" />
-            </SearchIcon>
-            <SearchField placeholder="Find Space" />
-        </SearchBarWrapper>
-    );
-};
 
 interface InputProps {
     formKey: string;
+    href?: string;
     textHandler;
 }
+
+export const SearchBarWithIcon: FunctionComponent<InputProps> = ({ formKey, href, textHandler }: InputProps) => {
+    return (
+        <SearchBarWrapper>
+            <LinkWrapper link={href}>
+                <SearchIcon>
+                    <Image img="/icons/search.svg" w="24px" h="24px" />
+                </SearchIcon>
+            </LinkWrapper>
+            <SearchField
+                placeholder="Find Space"
+                onChange={(event) => {
+                    textHandler(formKey, event.target.value);
+                }}
+            />
+        </SearchBarWrapper>
+    );
+};
 
 export const SearchBar: FunctionComponent<InputProps> = ({ formKey, textHandler }: InputProps) => {
     return (
--- components/SearchResult/index.tsx
+++ components/SearchResult/index.tsx
@@ -2,13 +2,14 @@ import React, { FunctionComponent } from 'react';
 import { SectionCaption, CardGallery } from '@components/ExploreSection/style';
 import { SearchSectionStyle } from './style';
 import MallCard from '@components/MallCard';
+import { useRouter } from 'next/router';
 
 const SearchResult: FunctionComponent = () => {
+    const router = useRouter();
+    const { location, contractType, spaceType } = router.query;
     return (
         <SearchSectionStyle>
-            <SectionCaption>
-                Search results for {'"'}Tebet, Jakarta Selatan{'"'}
-            </SectionCaption>
+            <SectionCaption>Search results for {`"${location}"`}</SectionCaption>
             <CardGallery>
                 <MallCard />
                 <MallCard />
